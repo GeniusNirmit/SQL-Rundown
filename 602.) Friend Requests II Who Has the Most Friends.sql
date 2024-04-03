7b4dbcf13ad01bf6132393d1db49b517a09919fb
Nirmit Pandya <nirmitrptp@gmail.com>
2024-04-04 00:34:42 +0530

    Solved

---
+++ 602.) Friend Requests II Who Has the Most Friends.sql
@@ -0,0 +1,31 @@
+WITH
+    all_ids AS (
+        SELECT
+            requester_id AS id
+        FROM
+            RequestAccepted
+        UNION ALL
+        SELECT
+            accepter_id AS id
+        FROM
+            RequestAccepted
+    )
+SELECT
+    id,
+    num
+FROM
+    (
+        SELECT
+            id,
+            COUNT(id) AS num,
+            RANK() OVER (
+                ORDER BY
+                    COUNT(id) DESC
+            ) AS rnk
+        FROM
+            all_ids
+        GROUP BY
+            id
+    ) FriendsCount
+WHERE
+    rnk = 1
